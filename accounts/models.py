54083a157ac53c3fae44fd2c183b6d4905d7c02d
Stanislav <stanislav.rudoi@gmail.com>
2021-08-18 09:21:05 +0300

    Lesson 26 Django permissions

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class CustomUser(AbstractUser):
@@ -12,4 +14,10 @@ class CustomUser(AbstractUser):
         return self.username
 
     class Meta(AbstractUser.Meta):
-        pass
+        permissions = [('view_statistics', 'View extra statistics')]
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user(sender, instance, created, **kwargs):
+    if created:
+        instance.groups.add(Group.objects.get(name='Users'))
